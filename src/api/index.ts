158cd388002c0107a61bd8edeff8d35fcd040a92
zplusfour <73612690+zplusfour@users.noreply.github.com>
2022-08-12 20:12:56 +0000

    💄 improved the styling

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,104 +1,113 @@
-import express from 'express';
-import Cryptr from 'cryptr';
-import cookieParser from 'cookie-parser';
-import { v4 as uuidv4 } from 'uuid';
-import { TodoInterface, User } from '../db/schemas';
+import express from "express";
+import Cryptr from "cryptr";
+import cookieParser from "cookie-parser";
+import { v4 as uuidv4 } from "uuid";
+import { TodoInterface, User } from "../db/schemas";
 const router = express.Router();
-const cryptr = new Cryptr('verysecretpasswordxkjwloi2xnrewi');
+const cryptr = new Cryptr("verysecretpasswordxkjwloi2xnrewi");
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
 router.use(cookieParser());
 
-router.get('/', (_req: express.Request, res: express.Response) => {
-	res.json({ message: 'OK', status: 200 });
+router.get("/", (_req: express.Request, res: express.Response) => {
+  res.json({ message: "OK", status: 200 });
 });
 
-router.post('/signup', async (req: express.Request, res: express.Response) => {
-	const { username, password } = req.body;
-	const enpassword = cryptr.encrypt(password);
+router.post("/signup", async (req: express.Request, res: express.Response) => {
+  const { username, password } = req.body;
+  const enpassword = cryptr.encrypt(password);
 
-	if (await User.findOne({ username })) {
-		res.json({ message: 'Username already exists', status: 400 });
-	} else {
-		const user = new User({
-			username,
-			password: enpassword,
-			todos: [
-				{
-					todoId: uuidv4(),
-					title: 'Welcome to tobox!',
-					content: '# Start editing now!',
-					date: new Date().toLocaleString()
-				}
-			]
-		});
+  if (await User.findOne({ username })) {
+    res.json({ message: "Username already exists", status: 400 });
+  } else {
+    const user = new User({
+      username,
+      password: enpassword,
+      todos: [
+        {
+          todoId: uuidv4(),
+          title: "Welcome to tobox!",
+          content: "# Start editing now!",
+          date: new Date().toLocaleDateString(),
+        },
+      ],
+    });
 
-		await user.save();
-		res.redirect('/signin');
-	}
+    await user.save();
+    res.redirect("/signin");
+  }
 });
 
-router.post('/signin', async (req: express.Request, res: express.Response) => {
-	const { username, password } = req.body;
-	const user = await User.findOne({ username });
-	if (user) {
-		if (cryptr.decrypt(user.password) === password) {
-			res.cookie('user', cryptr.encrypt(username));
-			res.redirect('/');
-		} else {
-			res.redirect('/signin');
-		}
-	} else {
-		res.redirect('/signin');
-	}
+router.post("/signin", async (req: express.Request, res: express.Response) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (user) {
+    if (cryptr.decrypt(user.password) === password) {
+      res.cookie("user", cryptr.encrypt(username));
+      res.redirect("/");
+    } else {
+      res.redirect("/signin");
+    }
+  } else {
+    res.redirect("/signin");
+  }
 });
 
-router.get('/del/:id', async (req: express.Request, res: express.Response) => {
-	const { id } = req.params;
-	const user = await User.findOne({ username: cryptr.decrypt(req.cookies['user']) });
-	if (user) {
-		const todos = user.todos.filter((todo) => todo.todoId !== id);
-		user.todos = todos;
-		await user.save();
-		res.redirect('/');
-	} else {
-		res.redirect('/signin');
-	}
+router.get("/del/:id", async (req: express.Request, res: express.Response) => {
+  const { id } = req.params;
+  const user = await User.findOne({
+    username: cryptr.decrypt(req.cookies["user"]),
+  });
+  if (user) {
+    const todos = user.todos.filter((todo) => todo.todoId !== id);
+    user.todos = todos;
+    await user.save();
+    res.redirect("/");
+  } else {
+    res.redirect("/signin");
+  }
 });
 
-router.post('/new', async (req: express.Request, res: express.Response) => {
-	const { title, content } = req.body;
-	const user = await User.findOne({ username: cryptr.decrypt(req.cookies['user']) });
-	if (user) {
-		const todo = {
-			todoId: uuidv4(),
-			title,
-			content,
-			date: new Date().toLocaleString()
-		};
-		user.todos.push(todo);
-		await user.save();
-		res.redirect('/');
-	} else {
-		res.redirect('/signin');
-	}
+router.post("/new", async (req: express.Request, res: express.Response) => {
+  const { title, content } = req.body;
+  const user = await User.findOne({
+    username: cryptr.decrypt(req.cookies["user"]),
+  });
+  if (user) {
+    const todo = {
+      todoId: uuidv4(),
+      title,
+      content,
+      date: new Date().toLocaleDateString(),
+    };
+    user.todos.push(todo);
+    await user.save();
+    res.redirect("/");
+  } else {
+    res.redirect("/signin");
+  }
 });
 
-router.post('/edit/:id', async (req: express.Request, res: express.Response) => {
-	const { id } = req.params;
-	const { title, content } = req.body;
-	const user = await User.findOne({ username: cryptr.decrypt(req.cookies['user']) });
-	if (user) {
-		let todo = user.todos.find((todo) => todo.todoId === id) as TodoInterface;
-		todo.title = title;
-		todo.content = content;
-		todo.date = new Date().toLocaleString() + ' (edited)';
-		await user.save();
-		res.redirect('/');
-	} else {
-		res.redirect('/signin');
-	}
-});
+router.post(
+  "/edit/:id",
+  async (req: express.Request, res: express.Response) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const user = await User.findOne({
+      username: cryptr.decrypt(req.cookies["user"]),
+    });
+    if (user) {
+      let todo = user.todos.find((todo) => todo.todoId === id) as TodoInterface;
+      todo.title = title;
+      todo.content = content;
+      todo.date = new Date().toLocaleDateString() + " (edited)";
+      await user.save();
+      res.redirect("/");
+    } else {
+      res.redirect("/signin");
+    }
+  }
+);
 
 export default router;
--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,85 @@
-import express from 'express';
-import ejs from 'ejs';
-import mongoose, { ObjectId } from 'mongoose';
+import express from "express";
+import ejs from "ejs";
+import mongoose, { ObjectId } from "mongoose";
 // import { marked } from 'marked';
-import { User } from './db/schemas';
-import dotenv from 'dotenv';
-import api from './api';
-import Cryptr from 'cryptr';
-import cookieParser from 'cookie-parser';
+import { User } from "./db/schemas";
+import dotenv from "dotenv";
+import api from "./api";
+import Cryptr from "cryptr";
+import cookieParser from "cookie-parser";
 const app = express();
-const cryptr = new Cryptr('verysecretpasswordxkjwloi2xnrewi');
+const cryptr = new Cryptr("verysecretpasswordxkjwloi2xnrewi");
 
 dotenv.config();
 
-app.set('view engine', 'html');
-app.engine('html', ejs.renderFile);
-app.use('/api', api);
+app.set("view engine", "html");
+app.engine("html", ejs.renderFile);
+app.use("/api", api);
 app.use(cookieParser());
 
 mongoose.connect(`${process.env.MDB_URI}`);
 
-app.get('/', async (req: express.Request, res: express.Response) => {
-	const user = req.cookies['user'];
-	if (user) {
-		res.render('index', { user: await User.findOne({ username: cryptr.decrypt(user) }) });
-	} else {
-		res.redirect('/signin');
-	}
+app.get("/", async (req: express.Request, res: express.Response) => {
+  const user = req.cookies["user"];
+  if (user) {
+    res.render("index", {
+      user: await User.findOne({ username: cryptr.decrypt(user) }),
+    });
+  } else {
+    res.redirect("/signin");
+  }
 });
 
-app.get('/signin', (req: express.Request, res: express.Response) => {
-	if (req.cookies['user']) {
-		res.redirect('/');
-	} else {
-		res.render('signin');
-	}
+app.get("/signin", (req: express.Request, res: express.Response) => {
+  if (req.cookies["user"]) {
+    res.redirect("/");
+  } else {
+    res.render("signin");
+  }
 });
 
-app.get('/signup', (req: express.Request, res: express.Response) => {
-	if (req.cookies['user']) {
-		res.redirect('/');
-	} else {
-		res.render('signup');
-	}
+app.get("/signup", (req: express.Request, res: express.Response) => {
+  if (req.cookies["user"]) {
+    res.redirect("/");
+  } else {
+    res.render("signup");
+  }
 });
 
-app.get('/new', async (req: express.Request, res: express.Response) => {
-	const user = req.cookies['user'];
-	if (user) {
-		res.render('new');
-	} else {
-		res.redirect('/signin');
-	}
+app.get("/new", async (req: express.Request, res: express.Response) => {
+  const user = req.cookies["user"];
+  if (user) {
+    res.render("new");
+  } else {
+    res.redirect("/signin");
+  }
 });
 
-app.get('/edit/:id', async (req: express.Request, res: express.Response) => {
-	const { id } = req.params;
-	const username = req.cookies['user'];
-	const user: any = await User.findOne({ username: cryptr.decrypt(username) });
-	const todo = user.todos.find(
-		(todo: { todoId: string; title: string; content: string; date: string; _id: ObjectId }) =>
-			todo.todoId === id
-	);
+app.get("/edit/:id", async (req: express.Request, res: express.Response) => {
+  const { id } = req.params;
+  const username = req.cookies["user"];
+  const user: any = await User.findOne({ username: cryptr.decrypt(username) });
+  const todo = user.todos.find(
+    (todo: {
+      todoId: string;
+      title: string;
+      content: string;
+      date: string;
+      _id: ObjectId;
+    }) => todo.todoId === id
+  );
 
 	if (username) {
-		res.render('edit', { user: await User.findOne({ username }), todo });
-	} else {
-		res.redirect('/signin');
+		if (typeof todo === "undefined") {
+			res.render("error", { err: 'Todo not found.' })
+		} else {
+    	res.render("edit", { user: await User.findOne({ username }), todo });
+		}
+  } else {
+  	res.redirect("/signin");
 	}
 });
 
 app.listen(8080, () => {
-	console.log('Listening on port 8080');
+  console.log("Listening on port 8080");
 });
